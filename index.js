d7dc1c07e85e0ca0db0d17788c0f305988c7bf25
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-05 16:03:51 -0400

    Adding inquirer questions

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
 const fs = require('fs');
+const inquirer = require("inquirer");
+const Htmlcode = require('./lib/Htmlcode');
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+const Htmlcode = require('./lib/Htmlcode')
 test = "Employee"
-const content = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${test}</title>
-    <h1>${test}</h1>
-</head>
-<body>
-    
-</body>
-</html>
-`;
+
+const testEmp = new Engineer('Employee', '01', 'test@email.com', "aalink")
+// const makeHtml = new Htmlcode('Link', 'Hero', 'hero@hyrule.com')
+// console.log(makeHtml.html)
+console.log(testEmp.name)
+console.log(testEmp.id)
+console.log(testEmp.email)
+console.log(testEmp.github)
+
 fileName = 'index.html'
-fs.writeFile(`./dist/${fileName}`, content, err => {
+fs.writeFile(`./dist/${fileName}`, Htmlcode(), err => {
   if (err) {
     console.error(err);
   }
   // file written successfully
 });
+
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "manager",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team manager's EMail",
+    },
+    {
+      type: "input",
+      name: "office",
+      message: "What is the team manager's office number?",
+    },
+    {
+      type: "list",
+      name: "addemployee",
+      message: "Which type of team member would you like to add?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "I don't want to add any more team members.",
+      ],
+    },
+  ]);
+};
+
+
+
+
+
+
+
+
